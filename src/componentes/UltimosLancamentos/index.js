3d82d6ed94ffcfd7388ea994ae5954f118183e06
Jean Cesar <neo.jean@gmail.com>
2023-07-26 09:18:31 -0300

    Projeto do curso de React da Alura

---
+++ src/componentes/UltimosLancamentos/index.js
@@ -0,0 +1,42 @@
+import { livros } from './dadosUltimosLancamentos';
+import { Titulo } from '../Titulo';
+import CardRecomenda from '../CardRecomenda';
+import { styled } from 'styled-components';
+import capaLivro from '../../images/livro2.png';
+
+const UltimosLancamentosContainer = styled.section`
+    background-color: #EBECEE;
+    padding-bottom: 20px;
+    display: flex;
+    flex-direction: column;
+`
+
+const NovosLivrosContainer = styled.div`
+    margin-top: 30px;
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    cursor: pointer;
+`
+
+export default function UltimosLancamentos() {
+    return (
+        <UltimosLancamentosContainer>
+            <Titulo
+                cor="#EB9B00"
+                tamanhoFonte="36px"
+            >ÚLTIMOS LANÇAMENTOS</Titulo>
+            <NovosLivrosContainer>
+                {livros.map(livro => (
+                    <img src={livro.src} alt={livro.nome} />
+                ))}
+            </NovosLivrosContainer>
+            <CardRecomenda
+                titulo="Talvez você se interesse por..."
+                subtitulo="Angular 11"
+                descricao="Construindo uma aplicação integrada com a plataforma Google."
+                img={capaLivro}
+            ></CardRecomenda>
+        </UltimosLancamentosContainer>
+    )
+}
